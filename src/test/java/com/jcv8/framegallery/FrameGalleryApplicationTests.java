bbf3bc1c9ffed2c4f6ee67f6302be7a27b0931b9
jcvb <github@jcvb.de>
2024-06-17 15:27:47 +0200

    fix test setup

--- src/test/java/com/jcv8/framegallery/FrameGalleryApplicationTests.java
+++ src/test/java/com/jcv8/framegallery/FrameGalleryApplicationTests.java
@@ -1,11 +1,17 @@
 package com.jcv8.framegallery;
 
 
+import main.java.com.jcv8.framegallery.FrameGalleryApplication;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.*;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
-class FrameGalleryApplicationTests {
+@SpringBootTest(classes = FrameGalleryApplication.class, webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+public class FrameGalleryApplicationTests {
 
     @Test
     void contextLoads() {
