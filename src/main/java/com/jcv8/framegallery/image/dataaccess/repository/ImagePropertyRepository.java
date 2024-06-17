a3e080e6053f6ef18ba65486ac52e4099ab2781a
jcvb <github@jcvb.de>
2024-06-17 21:15:58 +0200

    add ImagePropertyRepository.java

---
+++ src/main/java/com/jcv8/framegallery/image/dataaccess/repository/ImagePropertyRepository.java
@@ -0,0 +1,9 @@
+package main.java.com.jcv8.framegallery.image.dataaccess.repository;
+
+import main.java.com.jcv8.framegallery.image.dataaccess.entity.ImageProperty.ImageProperty;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ImagePropertyRepository extends JpaRepository<ImageProperty<?>, UUID> {
+}
