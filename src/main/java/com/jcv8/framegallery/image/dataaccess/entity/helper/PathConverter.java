9397fa752c2878e234440990c0881572d93fc1b2
jcvb <github@jcvb.de>
2024-06-17 21:15:29 +0200

    add PathConverter.java to allow Path as db value

---
+++ src/main/java/com/jcv8/framegallery/image/dataaccess/entity/helper/PathConverter.java
@@ -0,0 +1,27 @@
+package main.java.com.jcv8.framegallery.image.dataaccess.entity.helper;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+import main.java.com.jcv8.framegallery.fileStorage.StorageProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.nio.file.Path;
+
+@Converter(autoApply = true)
+public class PathConverter implements AttributeConverter<Path, String> {
+
+    private final StorageProperties properties = new StorageProperties();
+
+    @Override
+    public String convertToDatabaseColumn(Path path) {
+        if(path == null){
+            return "";
+        }
+        return properties.getLocation() + "/" + path.getFileName().toString();
+    }
+
+    @Override
+    public Path convertToEntityAttribute(String s) {
+        return Path.of(s);
+    }
+}
