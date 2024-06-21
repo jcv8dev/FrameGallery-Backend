7f85c210c2ab70071f2e20dc6b4f570284116102
jcvb <github@jcvb.de>
2024-06-21 11:47:36 +0200

    make auth for /image/all optional

--- src/main/java/com/jcv8/framegallery/image/facade/ImageController.java
+++ src/main/java/com/jcv8/framegallery/image/facade/ImageController.java
@@ -7,7 +7,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
